9ab6abcb7f129ab8b511f3b50c901a4175dd857b
Paul Wood III <paul.wood.iii@gmail.com>
2016-11-19 16:20:59 -0500

    init from the sails in action book

---
+++ api/controllers/GameprototypeController.js
@@ -0,0 +1,11 @@
+/**
+ * GameprototypeController
+ *
+ * @description :: Server-side logic for managing gameprototypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/GamestateController.js
@@ -0,0 +1,11 @@
+/**
+ * GamestateController
+ *
+ * @description :: Server-side logic for managing gamestates
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
